6348e7d6e75e7d469dcb3c9ebac8f3d1ef1b0678
Stav Pan Geffen <stavgeffen@gmail.com>
2015-12-22 20:04:14 +0200

    Prep for Heroku

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 'use strict';
 
-// apply environment variables
-const env = require('./env');
-for (let i in env){
-	process.env[i] = env[i];
-}
+var optional = require('optional');
+
+// apply local environment variables
+const env = optional('./env');
+if (env) for (let i in env) process.env[i] = env[i];
 
 let port = process.env.PORT || 3000;
-
 require('./server/server')
 	.init()
 	.listen(port, () => console.log(`Server is up! Listening on ${port}.`));
